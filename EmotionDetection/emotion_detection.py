8bb9078335ff3b5531ae474c572b4708fedbf0c8
Ngqabutho Dlamini <ngqaza.dlamz47@gmail.com>
2025-01-17 03:32:16 -0500

    Finished Tests

---
+++ EmotionDetection/emotion_detection.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+def emotion_detector(text_to_analyse):
+    url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
+    headers = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
+    input_json = { "raw_document": { "text": text_to_analyse } }
+
+    response =requests.post(url, json = input_json, headers = headers)
+
+    # Get the emotion dictionary from the response
+    response_dict = json.loads(response.text)
+    emotion_dict = response_dict["emotionPredictions"][0]["emotion"]
+
+    # Destructure the emotion dictionary
+    anger_score = emotion_dict["anger"]
+    disgust_score = emotion_dict["disgust"]
+    fear_score = emotion_dict["disgust"]
+    joy_score = emotion_dict["disgust"]
+    sadness_score = emotion_dict["disgust"]
+    dominant_emotion = ""
+    dominant_score = 0.0
+
+    # Determine the dominant emotion 
+    for emotion, emotion_score in emotion_dict.items():
+        if emotion_score > dominant_score:
+            dominant_score = emotion_score
+            dominant_emotion = emotion
+    
+    # Return the dictionary
+    return {
+        'anger': anger_score,
+        'disgust': disgust_score,
+        'fear': fear_score,
+        'joy': joy_score,
+        'sadness': sadness_score,
+        'dominant_emotion': dominant_emotion
+    }
